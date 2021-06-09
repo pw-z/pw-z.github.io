df3bb9bfe90969e5c03940df1478494cf1349835
Pengwei.Zhang <vilaseaka@outlook.com>
2021-06-09 19:20:36 +0800

    new solution

--- solution-of-HDU-OJ/1001.cpp
+++ solution-of-HDU-OJ/1001.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char const *argv[]){
+
+// Wrong answer, not that simple
+// int main(int argc, char const *argv[]){
+//     int lines;
+//     cin>>lines;
+//     long long A,B;
+//     for (int i = 0; i < lines; i++)
+//     {
+//         cin>>A>>B;
+//         cout<<"Case "<<i+1<<":"<<endl;
+//         cout<<A<<" + "<<B<<" = "<<A+B<<endl<<endl;
+//     }
+//     return 0;
+// }
+
+int main(int argc, char const *argv[])
+{
+    int lines;
+    cin>>lines;
+    char number_a[100], number_b[100];
+    for (int i = 0; i < lines; i++)
+    {
+        cin>>number_a>>number_b;
+        cout<<number_b;
+    }
     
     return 0;
 }
