9a80ba3b6ad1e8b65ae54a41d82497d189ba6755
Pengwei.Zhang <vilaseaka@outlook.com>
2021-06-20 16:43:25 +0800

    add new

--- datestructure-from-scratch/list0.h
+++ datestructure-from-scratch/list0.h
@@ -1,19 +1,30 @@
+#ifndef LIST0_H_
+#define LIST0_H_
+
 typedef int Item;
 
 class List
 {
 private:
-    enum {MAX = 10};
+    enum {MAX = 100};
     Item items[MAX];
 public:
     List();
     bool insert(int i, Item e);
     bool remove(int i);
-    
+    bool replace(int i, Item e);
+    Item get(int i);
+    int find(Item e);
+    void printlist();
+    void destroylist();
+    int length();
     bool isempty() const;
     bool isfull() const;
 };
 
 List::List(/* args */)
 {
+    
 }
+
+#endif
