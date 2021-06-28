44c1abfa354e449da1aedbd49f7a45861b5579d4
Pengwei.Zhang <vilaseaka@outlook.com>
2021-06-28 21:30:01 +0800

    temp backup

--- code/datastructure-from-scratch/list/5-singly-linkedlist/list4.h
+++ code/datastructure-from-scratch/list/5-singly-linkedlist/list4.h
@@ -17,7 +17,7 @@ class Node {
 template <class Item>
 class List {
    private:
-    Item* head;
+    Node<Item>* head;
     int length_;
 
    public:
@@ -37,104 +37,108 @@ class List {
 
 template <class Item>
 List<Item>::List() {
-    head = new Node;
-
-
+    head = new Node<Item>;
+    head->item = 999;
+    std::cout<<"head location: " << head << "\n";
     length_ = 0;
 }
 
 template <class Item>
-List<Item>::~List() {}
-
-template <class Item>
-bool List<Item>::insert(int i, Item e) {
-    if (i < 1 || i > length_ + 1)
-        return false;
-
-    ++length_;
-    return true;
+List<Item>::~List() {
+    // for item in length, delete node->next
+    std::cout<<"head item: " << head->item<<"\n";
+    delete head;
 }
 
-template <class Item>
-bool List<Item>::remove(int i) {
-    if (i < 1 || i > length_)
-        return false;
-    if (i == length_) {
-        --length_;
-        return true;
-    }
-    for (int index = i; index <= length_; ++index) {
-        p_items_[index - 1] = p_items_[index];
-    }
-    --length_;
-    return true;
-}
+// template <class Item>
+// bool List<Item>::insert(int i, Item e) {
+//     if (i < 1 || i > length_ + 1)
+//         return false;
 
-template <class Item>
-bool List<Item>::replace(int i, Item e) {
-    if (i<1 | i> length_)
-        return false;
-    p_items_[i - 1] = e;
-    return true;
-}
+//     ++length_;
+//     return true;
+// }
 
-template <class Item>
-bool List<Item>::get(int i, Item& e) {
-    if (i < 1 || i > length_) {
-        std::cout << "Error: invalid index\n";
-        // here must return something
-        return false;
-    } else {
-        e = p_items_[i - 1];
-        return true;
-    }
-}
+// template <class Item>
+// bool List<Item>::remove(int i) {
+//     if (i < 1 || i > length_)
+//         return false;
+//     if (i == length_) {
+//         --length_;
+//         return true;
+//     }
+//     for (int index = i; index <= length_; ++index) {
+//         p_items_[index - 1] = p_items_[index];
+//     }
+//     --length_;
+//     return true;
+// }
 
-template <class Item>
-int List<Item>::find(Item e) {
-    if (isempty())
-        return -1;
-    for (int i = 0; i < length_; ++i) {
-        if (p_items_[i] == e)
-            return i + 1;
-    }
-    return -1;
-}
+// template <class Item>
+// bool List<Item>::replace(int i, Item e) {
+//     if (i<1 | i> length_)
+//         return false;
+//     p_items_[i - 1] = e;
+//     return true;
+// }
 
-template <class Item>
-void List<Item>::printlist() const {  // updated
-    if (isempty()) {
-        std::cout << "Error: try to print an empty list\n";
-    } else {
-        for (int i = 0; i < length_; i++) {
-            if (i != 0 && i % 10 == 0)
-                std::cout << "\n";
-            std::cout << p_items_[i] << " ";
-        }
-        std::cout << "\n";
-    }
-}
+// template <class Item>
+// bool List<Item>::get(int i, Item& e) {
+//     if (i < 1 || i > length_) {
+//         std::cout << "Error: invalid index\n";
+//         // here must return something
+//         return false;
+//     } else {
+//         e = p_items_[i - 1];
+//         return true;
+//     }
+// }
 
-template <class Item>
-int List<Item>::length() {
-    return length_;
-}
+// template <class Item>
+// int List<Item>::find(Item e) {
+//     if (isempty())
+//         return -1;
+//     for (int i = 0; i < length_; ++i) {
+//         if (p_items_[i] == e)
+//             return i + 1;
+//     }
+//     return -1;
+// }
 
-template <class Item>
-bool List<Item>::isempty() const {
-    if (length_ == 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
+// template <class Item>
+// void List<Item>::printlist() const {  // updated
+//     if (isempty()) {
+//         std::cout << "Error: try to print an empty list\n";
+//     } else {
+//         for (int i = 0; i < length_; i++) {
+//             if (i != 0 && i % 10 == 0)
+//                 std::cout << "\n";
+//             std::cout << p_items_[i] << " ";
+//         }
+//         std::cout << "\n";
+//     }
+// }
 
-template <class Item>
-bool List<Item>::isfull() const {
-    if (length_ == capacity_) {
-        return true;
-    } else {
-        return false;
-    }
-}
+// template <class Item>
+// int List<Item>::length() {
+//     return length_;
+// }
+
+// template <class Item>
+// bool List<Item>::isempty() const {
+//     if (length_ == 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// template <class Item>
+// bool List<Item>::isfull() const {
+//     if (length_ == capacity_) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
 #endif
---
+++ code/datastructure-from-scratch/list/5-singly-linkedlist/test.cpp
@@ -0,0 +1,10 @@
+#include "list4.h"
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    using std::cout;
+
+    List<int> mylist = List<int>();
+    return 0;
+}
