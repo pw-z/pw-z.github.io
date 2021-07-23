36130056c1117ac7df120c8470adf9c04c5ef299
Pengwei.Zhang <vilaseaka@outlook.com>
2021-07-23 19:49:29 +0800

    temp

--- code/data-structure-from-scratch/2_limited_list/2-stack-linked-list-implementation/stack.h
+++ code/data-structure-from-scratch/2_limited_list/2-stack-linked-list-implementation/stack.h
@@ -5,14 +5,34 @@ static const int INIT_SIZE = 100;
 static const int EXPAND_SIZE = 100;
 
 template<class Item>
-class Stack
-{
-private:
+class Node{
+public:
+    Item data;
+    Node* next;
+};
 
+template<class Item>
+class Stack{
+private:
+    Node<Item>* p_base_;
+    Node<Item>* p_top_;
+    int length_;
     
 public:
     Stack();
     ~Stack();
+    Item pop();
+    bool push(Item item);
+    bool suicide();
+    bool clear();
+    bool isempty();
+    bool isfull();
+    int length();
 };
+
+template<class Item>
+Stack<Item>::Stack(){
+    length_ = 0;
+}
 
 #endif
