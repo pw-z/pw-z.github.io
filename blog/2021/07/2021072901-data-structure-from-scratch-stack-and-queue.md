755f4200014fb9a9ee054a1bac2ac42562f601dc
Pengwei.Zhang <vilaseaka@outlook.com>
2021-07-29 22:15:46 +0800

    new blog file, api-autotest-tool daily code archived

---
+++ blog/2021/07/2021072901-data-structure-from-scratch-stack-and-queue.md
@@ -0,0 +1,5 @@
+# Data Structure from Scratch: Stack & Queue
+
+*Posted on 2021.07.29 by [Pengwei Zhang](http://pwz.wiki) under [CC BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0/)* 
+
+
--- code/api-autotest-tool/pytest_allure_version/pytest/run_test.py
+++ code/api-autotest-tool/pytest_allure_version/pytest/run_test.py
@@ -11,29 +11,107 @@ from log_helper import *
 import datetime
 
 import pytest
+import pytest_assume
 import allure
 
 
-class TestAPI:
-
+# @pytest.fixture(scope="class")
+def init_testcase_data():
     configpath = './config.ini'
     para = Parameter(configpath)
+
+    # print(para.get_parameter('excel_path'))
+    # print(para.get_parameter('sheet_list'))
     excel_path = para.get_parameter('excel_path')
+    # print(excel_path)
     sheets = para.get_parameter('sheet_list')
 
     logger = init_logger(__name__)
+    # print(logger.level)
+
+    casehandler = CaseHandler(para)
+    shellhandler = ShellHandler(para)
+    sqlhandler = SQLHandler(para)
+
+    test_data = []
 
     for sheet in sheets:
         cases = read_excel(excel_path, sheet)
         # print(cases)
 
         for case in cases:
-            # logger.info('\n' + '=' * 100 + '\n Run Case # ' + case['CaseName'] + '\n' + '=' * 100)
+            # print(case)
 
-            # @allure.story(case['CaseName'])
-            @pytest.mark.parametrize("step", case['CaseSteps'])
-            def test_case(self, step):
-                allure.dynamic.title(step['CaseStep'])
+            for step in case['CaseSteps']:
+                step["CaseName"] = case['CaseName']
+                # print(step)
+                # step = (step,)
+                # print(type(step[0]))
+                test_data.append(step)
+    return test_data
+
+
+class Test:
+
+    # data = init_testcase_data()
+    # print(data)
+
+    configpath = './config.ini'
+    para = Parameter(configpath)
+
+    # print(para.get_parameter('excel_path'))
+    # print(para.get_parameter('sheet_list'))
+    excel_path = para.get_parameter('excel_path')
+    # print(excel_path)
+    sheets = para.get_parameter('sheet_list')
+
+    logger = init_logger(__name__)
+    # print(logger.level)
+
+    casehandler = CaseHandler(para)
+    shellhandler = ShellHandler(para)
+    # sqlhandler = SQLHandler(para)
+
+    test_data = []
+
+    for sheet in sheets:
+        cases = read_excel(excel_path, sheet)
+        # print(cases)
+
+        for case in cases:
+            # print(case)
+
+            for step in case['CaseSteps']:
+                step["CaseName"] = case['CaseName']
+                # print(step)
+                # step = (step,)
+                # print(type(step[0]))
+                test_data.append(step)
+
+    @pytest.mark.parametrize("step", test_data)
+    def test_case(self, step):
+        allure.dynamic.story(step['CaseName'])
+        allure.dynamic.title(step['CaseStep'])
+        # assert step['CaseName'] == '参数获取1'
+
+
+
+        if step['Run?'] != 'y':
+            # logger.info('SKIP CASE')
+            pytest.mark.skip()
+        else:
+            if step['ShellScript'] != '':
+                self.shellhandler.run(step)
+
+            if step['Body'] != '':
+                self.casehandler.run(step)
+                pytest.assume(1 == 2)
+                pytest.assume(3 == 2)
+            # if step['DQL'] != '':
+                # self.sqlhandler.run(step)
+
+    # shellhandler.close()
+    # sqlhandler.close()
 
 
 if __name__ == '__main__':
