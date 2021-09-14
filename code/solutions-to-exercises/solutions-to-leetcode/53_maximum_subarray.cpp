9659a633778578e093b9b24d0ccf8d6c4faab89f
Pengwei.Zhang <vilaseaka@outlook.com>
2021-09-15 01:47:19 +0800

    leetcode 53, temp code filling

---
+++ code/solutions-to-exercises/solutions-to-leetcode/53_maximum_subarray.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::vector;
+
+
+int maxSubArray(vector<int>& nums) {
+    int result = 1;
+    
+}
+
+
+void test(){
+    vector<int> nums {-2,1,-3,4,-1,2,1,-5,4};
+    cout<<maxSubArray(nums);
+}
+
+int main(int argc, char const *argv[])
+{
+    test();
+    return 0;
+}
