54330db16ad10ce9dd287cf2021e687876059a91
Pengwei.Zhang <vilaseaka@outlook.com>
2021-09-22 01:34:57 +0800

    temp archived

---
+++ code/solutions-to-exercises/solutions-to-leetcode/202_Happy_Number.cpp
@@ -0,0 +1,28 @@
+/* 题目示例：
+Example 1:
+
+Input: n = 19
+Output: true
+Explanation:
+1^2 + 9^2 = 82
+8^2 + 2^2 = 68
+6^2 + 8^2 = 100
+1^2 + 0^2 + 0^2 = 1
+
+Example 2:
+
+Input: n = 2
+Output: false
+
+作者：力扣 (LeetCode)
+链接：https://leetcode-cn.com/leetbook/read/hash-table/xh1k9i/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
+
+class Solution {
+public:
+    bool isHappy(int n) {
+
+    }
+};
