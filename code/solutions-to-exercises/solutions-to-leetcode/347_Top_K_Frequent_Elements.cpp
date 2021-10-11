baaa9e1317b012074c1aa08fe1d4bb1e17630a3b
Pengwei.Zhang <vilaseaka@outlook.com>
2021-10-12 00:11:55 +0800

    update leetcode 347

--- code/solutions-to-exercises/solutions-to-leetcode/347_Top_K_Frequent_Elements.cpp
+++ code/solutions-to-exercises/solutions-to-leetcode/347_Top_K_Frequent_Elements.cpp
@@ -5,6 +5,15 @@ using std::unordered_map;
 
 class Solution {
 public:
+    /*
+    O(nlogk)整体思路：
+        1. 数数，将结果存入hashmap，key=number，value=counts
+        2. 使用map的前K个元素的counts建立大小为K的小根堆
+        3. 使用map的后续元素不断更新小根堆
+    需要进行的工作：
+        1. 主流程，数数，建堆，更新堆
+        2. 实现小根堆的更新算法，即将小于堆顶的元素替换成堆顶，然后下沉到正确位置
+    */
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int,int> count;
         for(int &num: nums){
