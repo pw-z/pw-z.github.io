374f59f3afcfec8b70221a68baa74b11f8765633
Pengwei.Zhang <vilaseaka@outlook.com>
2022-03-25 23:04:58 +0800

    leetcode 189, try more

--- code/solutions-to-exercises/solutions-to-leetcode/specific_topic/algorithm_baisc/two_pointers/189_rotate_array.py
+++ code/solutions-to-exercises/solutions-to-leetcode/specific_topic/algorithm_baisc/two_pointers/189_rotate_array.py
@@ -8,7 +8,6 @@ def rotate(nums, k):
         round = math.gcd(count, k)
         for i in range(round):
             cur, buf = i, nums[i]
-            start = cur
             while True:
                 next = (cur + k) % count
                 buf, nums[next] = nums[next], buf
@@ -17,8 +16,44 @@ def rotate(nums, k):
                     break
 
 
+def rotate_(nums, k):
+    """反转数组"""
+    def reverse(arr):
+        """unnecessary function"""
+        print('reverse before: ' + str(arr))
+        arr[:] = arr[::-1]
+        print('id(arr): ' + str(id(arr)))
+        print('reverse  after: ' + str(arr))
+    # print('id(nums): ' + str(id(nums)))
+
+    k %= len(nums)
+    # reverse(nums)
+    # reverse(nums[:k])
+    # reverse(nums[k:])
+
+    print(nums)
+    nums[:] = nums[::-1]
+
+    # nums[:k] = nums[:k:-1]
+    # nums[k:] = nums[k::-1]
+    nums[:k] = nums[:k][::-1]
+    nums[k:] = nums[k:][::-1]
+    print(nums)
+
+
+def reverse(nums, k):
+    """not work in this way"""
+    print(nums)
+    nums[:k] = nums[:k][::-1]
+    nums[k:] = nums[k:][::-1]
+    nums[:] = nums[::-1]
+    print(nums)
+
 
 if __name__ == '__main__':
-    nums = [1,2,3,4,5,6,7]
-    rotate(nums, 3)
-    print(nums)
+    nums_ = [1,2,3,4,5,6,7]
+    # rotate(nums_, 3)
+    rotate_(nums_, 3)
+    # print(nums_)
+
+    # reverse(nums_, 3)
