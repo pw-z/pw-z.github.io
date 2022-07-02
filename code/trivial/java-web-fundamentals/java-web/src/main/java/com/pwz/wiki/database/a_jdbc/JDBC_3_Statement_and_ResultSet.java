e89e4b81b81cf551b6f14f2fd31fac52c6fa7bd6
Pengwei.Zhang <vilaseaka@outlook.com>
2022-07-03 02:21:27 +0800

    try more about jdbc ~ ResultSet, PreparedStatement

--- code/trivial/java-web-fundamentals/java-web/src/main/java/com/pwz/wiki/database/a_jdbc/JDBC_3_Statement_and_ResultSet.java
+++
@@ -1,4 +0,0 @@
-package com.pwz.wiki.database.a_jdbc;
-
-public class JDBC_3_Statement_and_ResultSet {
-}
--- code/trivial/java-web-fundamentals/java-web/src/main/java/com/pwz/wiki/database/a_jdbc/JDBC_4_PreparedStatement.java
+++
@@ -1,4 +0,0 @@
-package com.pwz.wiki.database.a_jdbc;
-
-public class JDBC_4_PreparedStatement {
-}
